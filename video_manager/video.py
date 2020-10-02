229c84e8401903146ba7e5a079cedf3454de542b
QuentinN42 <quentin.lieumont@gmail.com>
2020-10-02 09:42:28 +0200

    feature(video): init done
    can open multiples cams

---
+++ video_manager/video.py
@@ -0,0 +1,23 @@
+import cv2
+
+
+class Video:
+    """ Multiple video captures
+    
+    """
+    
+    def __init__(self, cams: dict):
+        """ Init the Video class
+        
+        Parameters
+        ----------
+        cams: dict
+            cam_name (str) -> open parameters (with cv2.VideoCapture)
+        """
+        self.cams = dict()
+        for cam_name, params in cams.items():
+            try:
+                self.cams[cam_name] = cv2.VideoCapture(*params)
+            except Exception:
+                # TODO(QuentinN42): raise more specific exception
+                raise
